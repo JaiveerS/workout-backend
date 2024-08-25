d56fb6fd55dbe4f58ee4538f7b25e2c0bde11eb6
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-25 12:59:41 -0400

    added password encoding

--- src/main/java/com/jaiveer/workout/user/UserService.java
+++ src/main/java/com/jaiveer/workout/user/UserService.java
@@ -3,6 +3,8 @@ package com.jaiveer.workout.user;
 import com.jaiveer.workout.program.Workout;
 import com.jaiveer.workout.program.WorkoutProgram;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,14 +12,17 @@ import java.util.List;
 @Service
 public class UserService {
     UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     //add user password encryption in user class
     public User registerUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
