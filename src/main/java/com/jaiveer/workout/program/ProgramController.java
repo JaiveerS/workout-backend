b8b8f6a63eeedf53831494af3d90ad09dfc092ca
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-26 13:41:42 -0400

    refactored to get username from path variable

--- src/main/java/com/jaiveer/workout/program/ProgramController.java
+++ src/main/java/com/jaiveer/workout/program/ProgramController.java
@@ -2,6 +2,7 @@ package com.jaiveer.workout.program;
 
 import com.jaiveer.workout.user.User;
 import com.jaiveer.workout.webClient.GeminiApiService;
+import com.jaiveer.workout.webClient.GeminiResponse;
 import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -36,13 +37,13 @@ public class ProgramController {
     //get user info
     //get user goals
     //send to gemini api then generate workout program
-    @PostMapping("/create")
-    public ResponseEntity<WorkoutProgram> generate(@RequestBody String username) {
-        return ResponseEntity.ok(programService.createWorkoutProgram(username));
+    @PostMapping("/create/{username}")
+    public ResponseEntity<WorkoutProgram> generate(@RequestBody String prompt,@PathVariable String username) {
+        return ResponseEntity.ok(programService.createWorkoutProgram(prompt, username));
     }
 
     @PostMapping("/generateContent")
-    public String generateContent(@RequestBody String prompt) {
-        return geminiApiService.generateContent(prompt).block();
+    public GeminiResponse generateContent(@RequestBody String prompt) {
+        return geminiApiService.generateContent(prompt);
     }
 }
