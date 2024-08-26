4edd2c254613f0f5baefc59dda827983ac5591da
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-26 11:57:59 -0400

    createWorkoutProgram from gemini and save

--- src/main/java/com/jaiveer/workout/program/ProgramService.java
+++ src/main/java/com/jaiveer/workout/program/ProgramService.java
@@ -1,12 +1,17 @@
 package com.jaiveer.workout.program;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.jaiveer.workout.user.User;
 import com.jaiveer.workout.user.UserService;
+import com.jaiveer.workout.webClient.GeminiApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,12 +19,14 @@ import java.util.List;
 public class ProgramService {
     private final WorkoutProgramRepository workoutProgramRepository;
     private final UserService userService;
+    private final GeminiApiService geminiApiService;
 
 
     @Autowired
-    public ProgramService (WorkoutProgramRepository workoutProgramRepository, UserService userService) {
+    public ProgramService (WorkoutProgramRepository workoutProgramRepository, UserService userService, GeminiApiService geminiApiService) {
         this.workoutProgramRepository = workoutProgramRepository;
         this.userService = userService;
+        this.geminiApiService = geminiApiService;
     }
 
     //ask gemini ai to make a workout program and return it in json of a specifed format
@@ -29,27 +36,14 @@ public class ProgramService {
         User user = userService.getUser(username);
         List<WorkoutProgram> workoutPrograms = userService.getUserProgram(username);
 
-        //ask gemini to create workout program json format
-        //workoutprogram has these field
-        //then workout has these
-        //then exercise has these
-        //then I just iterate over and create a program correct?
+        Mono<String> program = geminiApiService.generateContent("create a 6 day a week workout program for me. I want it to be a push pull legs split. use current best standards based on the scientific literature for the optimal number of sets per bodypart for both hypertrophy and strength gains. for exercises use science backed best exercises that are attributed the most amount of muscle growth while also taking into account recovery. for rest times provide me with rest times based on how tiring the previous exercise is cosidered in terms of systemic load and based on what the science says is the optimal rest time for muscle growth. make the first batch of push pull leg workouts different from the second.");
+        Mono<String> text = extractText(program);
+        //comment this out
+        System.out.println(text.block());
 
-        WorkoutProgram workoutProgram = new WorkoutProgram();
-        workoutProgram.setName("test");
-        workoutProgram.setUserId(user.getUserid());
 
-        Workout workout = new Workout();
-        List<Workout> workouts = new ArrayList<Workout>();
-        workouts.add(workout);
-        List<Exercise> exercises = new ArrayList<>();
-        Exercise exercise = new Exercise();
-        exercise.setExerciseName("test");
-        exercise.setRepetitions("8-12");
-        exercise.setSets(3);
-        exercises.add(exercise);
-        workout.setExercises(exercises);
-        workoutProgram.setWorkouts(workouts);
+        WorkoutProgram workoutProgram = saveWorkoutProgram(text.block(), user);
+
         workoutPrograms.add(workoutProgram);
         user.setWorkoutPrograms(workoutPrograms);
         userService.updateUser(user);
@@ -60,4 +54,48 @@ public class ProgramService {
     List<WorkoutProgram> getAllWorkoutPrograms() {
         return workoutProgramRepository.findAll();
     }
+
+    public Mono<String> extractText(Mono<String> jsonMono) {
+        return jsonMono.flatMap(jsonString -> {
+            try {
+                JsonElement rootElement = JsonParser.parseString(jsonString);
+                JsonObject rootObject = rootElement.getAsJsonObject();
+                JsonObject firstCandidate = rootObject.getAsJsonArray("candidates").get(0).getAsJsonObject();
+                JsonObject contentObject = firstCandidate.getAsJsonObject("content");
+                JsonObject partsObject = contentObject.getAsJsonArray("parts").get(0).getAsJsonObject();
+                String text = partsObject.get("text").getAsString();
+                return Mono.just(text);
+            } catch (Exception e) {
+                return Mono.error(e);
+            }
+        });
+    }
+
+    public WorkoutProgram saveWorkoutProgram(String jsonData, User user) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            System.out.println("here start");
+            WorkoutProgram workoutProgram = objectMapper.readValue(jsonData, WorkoutProgram.class);
+            System.out.println("2");
+            List<WorkoutProgram> workoutPrograms = user.getWorkoutPrograms();
+            System.out.println("3");
+
+            workoutProgram.setUserId(user.getUserid());
+            System.out.println("4");
+
+            workoutPrograms.add(workoutProgram);
+            System.out.println("5");
+
+            user.setWorkoutPrograms(workoutPrograms);
+            userService.updateUser(user);
+            System.out.println("Data saved successfully!");
+            return workoutProgram;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error saving data: " + e.getMessage());
+        }
+        return null;
+    }
+
 }
