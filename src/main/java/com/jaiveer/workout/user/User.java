fe1f9c6e93abc33b50e2b6ccc2e248b80f5c9d6d
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-28 14:55:12 -0400

    setup authentication configurations

--- src/main/java/com/jaiveer/workout/user/User.java
+++ src/main/java/com/jaiveer/workout/user/User.java
@@ -2,42 +2,63 @@ package com.jaiveer.workout.user;
 
 import com.jaiveer.workout.program.WorkoutProgram;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+@Builder
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
     private Long userid;
+
     @Column(unique = true,nullable  = false)
     private String username;
+
     @Column(nullable = false)
     private String password;
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String firstname;
+
     @Column(nullable = false)
     private String lastname;
+
     private int age;
     private String gender;
     private double weight;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<WorkoutProgram> workoutPrograms;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>(Collections.singleton(Role.ROLE_USER));
 
     @Override
     public String toString(){
         return username + " " + firstname + " " + lastname + " " + age + " " + gender + " " + weight + " " + email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                        .collect(Collectors.toSet());
     }
 }
