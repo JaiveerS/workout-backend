0791614ffcd7d87dd098b92f15a7a7082f0a88ab
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-28 23:32:17 -0400

    added filter and required authentications

--- src/main/java/com/jaiveer/workout/config/SecurityConfig.java
+++ src/main/java/com/jaiveer/workout/config/SecurityConfig.java
@@ -1,27 +1,45 @@
 package com.jaiveer.workout.config;
 
-import com.jaiveer.workout.user.UserRepository;
+import com.jaiveer.workout.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        // Allow all requests to be authorized
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()  // Authorize all requests
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/user/register", "/api/user/login").permitAll()
+                        .requestMatchers("api/program/create/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable);  // Disable CSRF protection done for stateless rest apis
-
-        return http.build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(Customizer.withDefaults())
+                .build();
     }
 
 }
